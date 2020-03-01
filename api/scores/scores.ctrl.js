fd1a4f939f810df9e6439c7fe5f4330bca723b46
ifjso <ojsboy@hotmail.com>
2020-03-01 19:02:48 +0900

    score 조회 시 user 정보도 함께 주도록 수정

--- api/scores/scores.ctrl.js
+++ api/scores/scores.ctrl.js
@@ -1,15 +1,23 @@
 const createError = require('http-errors');
 const Score = require('../../models/score');
+const User = require('../../models/user');
+const History = require('../../models/history');
 
 const read = async (req, res, next) => {
   try {
     const { owner } = req.params;
+
+    const user = await user.findOne({ owner });
+    if (!user) {
+      throw new Error(`${owner} not found.`);
+    }
+
     const score = await Score.findOne({ owner });
     if (!score) {
       throw new createError.NotFound(`${owner}'s score not found.`);
     }
 
-    res.json(score);
+    res.json({ ...score, user });
   } catch (e) {
     next(e);
   }
@@ -19,6 +27,12 @@ const update = async (req, res, next) => {
   try {
     const { owner } = req.params;
     const { score } = req.body;
+
+    const user = await User.findOne({ owner });
+    if (!user) {
+      throw new Error(`${owner} not found.`);
+    }
+
     const prevScore = await Score.findOneAndUpdate(
       { owner },
       { score, updatedAt: Date.now() }
@@ -29,6 +43,12 @@ const update = async (req, res, next) => {
         `Failed to save ${owner}'s score.`
       );
     }
+
+    await History.create({
+      owner,
+      name: user.name,
+      value: score - prevScore.score
+    });
 
     res.json(Object.assign(prevScore, { score }));
   } catch (e) {
