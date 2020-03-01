3953edf4a622634de01aa7afd31085f842c8215a
ifjso <ojsboy@hotmail.com>
2020-03-01 19:02:36 +0900

    히스토리 쓰기 api 삭제

--- api/histories/histories.ctrl.js
+++ api/histories/histories.ctrl.js
@@ -1,21 +1,6 @@
 const History = require('../../models/history');
 const User = require('../../models/user');
 
-const write = async (req, res, next) => {
-  try {
-    const { owner, value } = req.body;
-    const user = await User.findOne({ owner });
-    if (!user) {
-      throw new Error(`${owner} not found.`);
-    }
-
-    const history = await History.create({ owner, name: user.name, value });
-    res.json(history);
-  } catch (e) {
-    next(e);
-  }
-};
-
 const list = async (req, res, next) => {
   try {
     const { from, to, skip, limit } = req.query;
