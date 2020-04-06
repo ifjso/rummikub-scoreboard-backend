caa3260566b8be52606ea7b2578162ee5db0e4dc
ifjso <ojsboy@hotmail.com>
2020-04-06 21:28:05 +0900

    picture images 추가

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/static', express.static(`${__dirname}/public`));
 
 const { API_CONTEXT } = process.env;
 app.use(`${API_CONTEXT}/histories`, historesRouter);
